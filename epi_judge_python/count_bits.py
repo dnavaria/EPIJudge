55e93c6eb09f7a89c59532659994df9d1faa367b
dnavaria <dk17816@gmail.com>
2022-03-29 13:10:56 +0530

    count_bits ✅

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def count_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    
+    # Using Brian keringhan's Algorithms
+    """
+        - Flipping the rightmost bit in each iterations
+        - Ex: x = 5
+            - itreration 1: x = 5 => 101, 5 & 4 = 4, cnt = 1, x = 4
+            - itreration 2: x = 4 => 101, 4 & 3 = 3, cnt = 2, x = 0
+            - Exit
+    """
+    cnt = 0
+    while x:
+        x = x & (x -1)
+        cnt+=1
+    return cnt
 
 
 if __name__ == '__main__':
